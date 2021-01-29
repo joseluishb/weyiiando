1b3b7cf39b31e524ae8fd188a303f926b56aab34
root <root@cb422e13341a>
2021-01-29 19:37:41 +0000

    extraFields change

--- frontend/resource/Post.php
+++ frontend/resource/Post.php
@@ -7,6 +7,11 @@ class Post extends \common\models\Post
 
     public function fields()
     {
-        return ['id', 'title', 'body', 'created_at'];
+        return ['id', 'title', 'body'];
+    }
+
+    public function extraFields()
+    {
+        return ['created_at', 'updated_at', 'updated_by'];
     }
 }
