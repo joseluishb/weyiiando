51f45b138fb25566004652c80ec78ad67139544b
root <root@cb422e13341a>
2021-01-30 19:17:44 +0000

    change some comment fields in Comment model and getting comments in post

--- frontend/resource/Comment.php
+++ frontend/resource/Comment.php
@@ -4,6 +4,11 @@ namespace frontend\resource;
 class Comment extends \common\models\Comment
 {
 
+    public function fields()
+    {
+        return ['id', 'title', 'body'];
+    }
+
 
 
 }
--- frontend/resource/Post.php
+++ frontend/resource/Post.php
@@ -7,11 +7,21 @@ class Post extends \common\models\Post
 
     public function fields()
     {
-        return ['id', 'title', 'body', 'comments'];
+        return ['id', 'title', 'body'];
     }
 
     public function extraFields()
     {
-        return ['created_at', 'updated_at', 'updated_by'];
+        return ['comments', 'created_at', 'updated_at', 'updated_by'];
+    }
+
+        /**
+     * Gets query for [[Comments]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\CommentQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::class, ['post_id' => 'id']);
     }
 }
